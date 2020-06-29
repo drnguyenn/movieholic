0bcb1cabf96c5d9c10746c82004489a8bfbb5657
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-29 16:03:12 +0700

    add: initialize movie-addition-form component

---
+++ src/components/movie-addition-form/movie-addition-form.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { MovieAdditionFormContainer } from './movie-addition-form.styles';
+
+const MovieAdditionForm = () => {
+  return <MovieAdditionFormContainer></MovieAdditionFormContainer>;
+};
+
+export default MovieAdditionForm;
